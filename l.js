b52be87b36498d066ef697f7d2c66f3657aab715
g1ts <coder@g1ts.net>
2012-12-06 19:09:48 +0600

    added mutation

--- l.js
+++ l.js
@@ -1,18 +1,11 @@
-// s = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADQAAAAaCAQAAABS17cmAAAAAXNSR0IArs4c6QAAAAJiS0dEAP+Hj8y/AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH3AwGCDEUmydGXgAAAAh0RVh0Q29tbWVudAD2zJa/AAAA00lEQVRIx7WW3Q3DIAyEbwnG6AYZmREqMQqjXB9oSxrh1AcmT1Ei/Pn3DEAwsXLfkwmCAPeiMh8N9Hk5+NyAKW/riejMeFRhzxeuH8Jrw4OVbKCOiovqmidYP2Iw6WsNtg8R0fRS4C7cmKQNQBEoywLs7NZAzAC0grrLB9QDHhUYnYLu238VoB+kqkX5UQEJpKCyQ1ewkg5LBWSQp1beHsVKyyqjgJXpKANNWwBZqCytFmhT0n1XJw1z8lLkgcaMkuWJfQxKatYA/haYBJ1uafLGegHmwLVw17GPCwAAAABJRU5ErkJggg==';
-
-//i = new Image(s);
-
-
-// картинки можно генерить на холсте или из base64 строк
+var imgs = [];
+var positions = [];
+var l = 26;
+var img = ['1.png', '2.png'];
+var _i=null;
 
 function mkbg(){
   var b = document.getElementsByTagName('body')[0];
-
-  var imgs = [];
-  var positions = [];
-
-  var l = 26;
-  var img = ['1.png', '2.png'];
   var sw = b.scrollWidth;
   var sh = b.scrollHeight;
 
@@ -22,9 +15,21 @@ function mkbg(){
       positions.push(j*l+'px '+i*l+'px');
     }
   }
+  setBg();
+  if(_i){clearInterval(_i);}
+  _i = setInterval(changeRndTile,500);
+}
 
+function changeRndTile(){
+  for(var i=0;i<imgs.length*0.05;i++){
+    imgs[Math.floor(imgs.length*Math.random())]=('url('+img[Math.round(Math.random())]+')');
+  }
+  setBg();
+}
+
+function setBg(){
+  var b = document.getElementsByTagName('body')[0];
   b.style.backgroundImage = imgs.join(', ');
   b.style.backgroundPosition = positions.join(', ');
   b.style.backgroundRepeat = "no-repeat";
 }
-
